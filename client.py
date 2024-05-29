bc97d6ca13ece1e0750eb0738e4eab40b7b2d0b3
Ксю C. <brooksdi@student-21.school.ru>
2024-05-29 15:16:11 +0300

    lead lab6

--- client.py
+++ client.py
@@ -1,14 +1,38 @@
 import socket
 import pickle
+from random import randint
 
-HOST = '127.0.0.1'
-PORT = 8080
+def encrypt_decrypt(message, key):
+    return ''.join(chr(ord(char) ^ key) for char in message)
+
+def send_message(sock, message, key):
+    encrypted_message = encrypt_decrypt(message, key)
+    sock.send(pickle.dumps(encrypted_message))
+
+def receive_message(sock, key):
+    encrypted_message = pickle.loads(sock.recv(1024))
+    return encrypt_decrypt(encrypted_message, key)
+
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 8080
 
 sock = socket.socket()
-sock.connect((HOST, PORT))
+sock.connect((SERVER_HOST, SERVER_PORT))
+print('Соединение установлено!')
+print('Введите "exit" для закрытия соединения')
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+prime, base, client_secret = [randint(0, 250) for _ in range(3)]
+client_public = pow(base, client_secret, prime)
+sock.send(pickle.dumps((prime, base, client_public)))
+server_public = pickle.loads(sock.recv(1024))
+shared_key = pow(server_public, client_secret, prime)
+
+while True:
+    message = input('Введите сообщение: ')
+    if message == 'exit':
+        break
+    send_message(sock, message, shared_key)
+    response = receive_message(sock, shared_key)
+    print(response)
 
 sock.close()
--- server.py
+++ server.py
@@ -1,15 +1,38 @@
 import socket
 import pickle
+from random import randint
 
-HOST = '127.0.0.1'
-PORT = 8080
+def encrypt_decrypt(message, key):
+    return ''.join(chr(ord(char) ^ key) for char in message)
+
+def send_message(conn, message, key):
+    encrypted_message = encrypt_decrypt(message, key)
+    conn.send(pickle.dumps(encrypted_message))
+
+def receive_message(conn, key):
+    encrypted_message = pickle.loads(conn.recv(1024))
+    return encrypt_decrypt(encrypted_message, key)
+
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 8080
 
 sock = socket.socket()
-sock.bind((HOST, PORT))
+sock.bind((SERVER_HOST, SERVER_PORT))
 sock.listen(1)
 conn, addr = sock.accept()
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+prime, base, client_public = pickle.loads(conn.recv(1024))
+server_secret = randint(10, 250)
+server_public = pow(base, server_secret, prime)
+conn.send(pickle.dumps(server_public))
+shared_key = pow(client_public, server_secret, prime)
+
+while True:
+    try:
+        message = receive_message(conn, shared_key)
+        print(message)
+        send_message(conn, 'сообщение получено', shared_key)
+    except (EOFError, ConnectionResetError):
+        break
 
 conn.close()
